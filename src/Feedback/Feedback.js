98642e4916a5fb604def89d6c567fbe22f8749a6
Oleksii83 <zaaytav@gmail.com>
2021-08-21 17:54:20 +0300

    Task 1/3 ok

--- src/Feedback/Feedback.js
+++ src/Feedback/Feedback.js
@@ -1,63 +1,64 @@
 import React from 'react';
+import Options from './feedbackOtions/FeedbackOptions';
 import './Feedback.css';
+import Statistics from './Statistics/FeedbackStatistics';
+import Section from './Section title/SectionTitle';
 
 class Feedback extends React.Component {
   state = {
     good: 0,
     neutral: 0,
     bad: 0,
+    visible: false,
   };
 
   clickGood = () => {
-    this.setState(prevState => {
-      return {
-        Good: prevState.Good + 1,
-      };
-    });
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }));
   };
 
   clickNeutral = () => {
-    this.setState(prevState => {
-      return {
-        Neutral: prevState.Neutral + 1,
-      };
-    });
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }));
   };
 
   clickBad = () => {
-    this.setState(prevState => {
-      return {
-        Bad: prevState.Bad + 1,
-      };
-    });
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good * 100) / this.countTotalFeedback());
   };
 
   render() {
     return (
-      <div className="Feedback">
-        <p className="FeedbackTitle"> Please Leave feedback </p>
-        <div className="FeedbackButtonContainer">
-          <button type="button" id="1" className="FeedbackButton" onClick={this.clickGood}>
-            Good
-          </button>
-          <button type="button" id="2" className="FeedbackButton" onClick={this.clickNeutral}>
-            Neutral
-          </button>
-          <button type="button" id="3" className="FeedbackButton" onClick={this.clickBad}>
-            Bad
-          </button>
-        </div>
-        <div className="FeedbackStatistic">
-          <p className="FeedbackStatisticTitle"> Statistic </p>
-          <div className="FeedbackStatisticSynopsis">
-            <ul className="FeedbackStatisticList">
-              <li className="FeedbackStatisticItem"> Good: {this.state.Good} </li>
-              <li className="FeedbackStatisticItem"> Neutral: {this.state.Neutral}</li>
-              <li className="FeedbackStatisticItem"> Bad: {this.state.Bad}</li>
-            </ul>
-          </div>
-        </div>
-      </div>
+      <section className="Feedback">
+        <Section title={'Please Leave feedback'} />
+        <Options
+          onClickGood={this.clickGood}
+          onClickNeutral={this.clickNeutral}
+          onClickBad={this.clickBad}
+        />
+
+        <Section title={'Statistic'} />
+
+        <Statistics
+          stateGood={this.state.good}
+          stateNeutral={this.state.neutral}
+          stateBad={this.state.bad}
+          stateTotal={this.countTotalFeedback()}
+          PositiveFeedback={this.countPositiveFeedbackPercentage()}
+        />
+      </section>
+      // <Notification message="No feedback given">
     );
   }
 }
